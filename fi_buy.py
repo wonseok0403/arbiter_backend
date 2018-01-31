7f90afa6119c4a8e22bcf9d708e9eeacfd56d106
MH-Lee <lmhoon012@gmail.com>
2018-01-31 08:57:45 +0000

    buySell

--- fi_buy.py
+++ fi_buy.py
@@ -60,7 +60,7 @@ def ticker():
             stockinst = {'date':date, 'name':name, 'code':code, 'market_type':market_type}
             stock.append(stockinst)
         page = page + 1
-    
+
 stock = ticker()
 
 
@@ -101,40 +101,40 @@ buyselltoday()
 # In[36]:
 
 def buyselltotal():
+    start = time.time()
     today = datetime.now()
-    one_years_ago = today-timedelta(days=365)
-    one_years_ago = one_years_ago.strftime('%Y%m%d')
-    one_years_ago
+    one_year_ago = today-timedelta(days=365)
+    one_year_ago = one_year_ago.strftime('%Y%m%d')
+    one_year_ago
     data_list = []
     page = 1
-    while 1:
-        url = 'http://finance.naver.com/item/frgn.nhn?code=058820&page='+ str(page)
-        print(url)
-        user_agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'}
-        r = requests.get(url, headers= user_agent, auth=('user', 'pass'))
-        soup = BeautifulSoup(r.text, 'html.parser')
-        tmp = soup.findAll('table',{'class':'type2'})
-        table = soup.findAll('tr',{'onmouseout':'mouseOut(this)'})
-        for i in range(len(table)):
-            date=table[i].find('span',{'class':'tah p10 gray03'}).text.replace('.','')
-            if date <= one_years_ago:
-                return data_list
-            institustion = table[i].findAll('td')[5].text
-            if type(institustion) == int:
-                institustion = institustion
-            else:
-                institustion = institustion.replace(',','')
-            foriegn = table[0].findAll('td')[6].text
-            if type(foriegn) == int:
-                foriegn = foriegn
-            else:
-                foriegn = foriegn.replace(',','')
-            tmp = {'date':date, 'code':'058820', 'institustion':institustion, 'foriegn':foriegn}
-            data_list.append(tmp)
-        page = page+1
-
-
-# In[37]:
-
-buysell()
-
+    for i in range(len(stock)):
+        code = stock[i]['code']
+        page = 1
+        while page:
+            url = 'http://finance.naver.com/item/frgn.nhn?code={}&page={}'.format(code,str(page))
+            print(url)
+            user_agent = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.95 Safari/537.36'}
+            r = requests.get(url, headers= user_agent, auth=('user', 'pass'))
+            soup = BeautifulSoup(r.text, 'html.parser')
+            tmp = soup.findAll('table',{'class':'type2'})
+            table = soup.findAll('tr',{'onmouseout':'mouseOut(this)'})
+            for i in range(len(table)):
+                date=table[i].find('span',{'class':'tah p10 gray03'}).text.replace('.','')
+                if date <= one_year_ago:
+                    break
+                institustion = table[i].findAll('td')[5].text
+                if type(institustion) == int:
+                    institustion = institustion
+                else:
+                    institustion = institustion.replace(',','')
+                foriegn = table[0].findAll('td')[6].text
+                if type(foriegn) == int:
+                    foriegn = foriegn
+                else:
+                    foriegn = foriegn.replace(',','')
+                tmp = {'date':date, 'code':'058820', 'institustion':institustion, 'foriegn':foriegn}
+                data_list.append(tmp)
+            page = 0 if date <= one_year_ago else page+1
+    end = time.time()
+    return end-start, data_list
